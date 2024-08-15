99cb04ddac0925522607750d2f820eb653a45c63
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-15 17:30:46 -0400

    feat: add startup logs

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+console.log("Hi! Welcome to the mini me project!");
+console.log(
+  "Please make sure you stuck your skype export into the data directory"
+);
+
+console.log("------------------\n");
+
+console.log("transforming the data into a valid training file!");

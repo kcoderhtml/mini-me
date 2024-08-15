f1fca46ea3d3f8a9ab48ff0d106a728e6c6ccfd5
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-15 19:32:30 -0400

    feat: use a try catch block and a main function

--- index.ts
+++ index.ts
@@ -2,57 +2,78 @@ import { createSelection, type SelectionItem } from "bun-promptx";
 import type { SkypeType } from "./type";
 import { Spinner } from "@topcli/spinner";
 
-console.log("Hi! Welcome to the mini me project!");
-console.log(
-  "Please make sure you stuck your skype export into the data directory"
-);
+(async function main() {
+  try {
+    console.log("Hi! Welcome to the mini me project!");
+    console.log(
+      "Please make sure you stuck your skype export into the data directory"
+    );
 
-console.log("------------------\n");
-const spinner = new Spinner().start("loading json data!");
-await Bun.sleep(1000);
+    console.log("------------------\n");
+    const spinner = new Spinner().start("loading json data!");
+    await Bun.sleep(1000);
 
-const messageData: SkypeType = await Bun.file("data/messages.json").json();
+    const messageData: SkypeType = await Bun.file("data/messages.json").json();
 
-spinner.succeed(
-  "You have " +
-    messageData.conversations.length +
-    " conversations included in your export"
-);
-console.log(
-  "\x1b[92m✔\x1b[0m You have " +
-    messageData.conversations.reduce(
-      (total, conversation) => total + conversation.MessageList.length,
-      0
-    ) +
-    " messages included in your export"
-);
+    spinner.succeed(
+      "You have " +
+        messageData.conversations.length +
+        " conversations included in your export"
+    );
+    console.log(
+      "\x1b[92m✔\x1b[0m You have " +
+        messageData.conversations.reduce(
+          (total, conversation) => total + conversation.MessageList.length,
+          0
+        ) +
+        " messages included in your export"
+    );
 
-console.log("\n\n------------------\n");
+    console.log("\n\n------------------\n");
 
-const result = createSelection(
-  messageData.conversations.map((conversation) => {
-    return {
-      text: conversation.id + ":",
-      description: conversation.displayName || "null",
-    };
-  }) as SelectionItem[],
-  {
-    headerText: "Select the conversation you want to use as training data: ",
-    perPage: 5,
-    footerText: "Press enter when you are ready",
+    const result = createSelection(
+      messageData.conversations.map((conversation) => {
+        return {
+          text: conversation.id + ":",
+          description: conversation.displayName || "null",
+        };
+      }) as SelectionItem[],
+      {
+        headerText:
+          "Select the conversation you want to use as training data: ",
+        perPage: 5,
+        footerText: "Press enter when you are ready",
+      }
+    );
+    if (result.error || result.selectedIndex === null) {
+      console.error("Something went wrong:", result.error);
+      process.exit(1);
+    }
+
+    const selectedConversation =
+      messageData.conversations[result.selectedIndex];
+
+    console.log(
+      "You selected",
+      `\`${selectedConversation.displayName || selectedConversation.id}\``,
+      "with",
+      selectedConversation.MessageList.length,
+      "messages in it"
+    );
+
+    console.log(
+      "the data spans from",
+      new Date(
+        selectedConversation.MessageList[
+          selectedConversation.MessageList.length - 1
+        ].originalarrivaltime
+      ).toDateString(),
+      "to",
+      new Date(
+        selectedConversation.MessageList[0].originalarrivaltime
+      ).toDateString()
+    );
+  } catch (e) {
+    console.error(e);
   }
-);
-if (result.error || result.selectedIndex === null) {
-  console.error("Something went wrong:", result.error);
-  process.exit(1);
-}
-
-const selectedConversation = messageData.conversations[result.selectedIndex];
-
-console.log(
-  "You selected",
-  `\`${selectedConversation.displayName || selectedConversation.id}\``,
-  "with",
-  selectedConversation.MessageList.length,
-  "messages in it"
-);
+})();
